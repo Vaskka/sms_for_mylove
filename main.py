0584a49ae9287adec7ad7ad907d8647c0b5de3dc
Vaskka <1139851358@qq.com>
2018-12-25 01:48:51 +0800

    add main function

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-import json
 import time
 
 import schedule
 
-import core
-import model
-import settings
 from core.main_core import LittleSa
-from core.weather import WeatherCatcher
 from settings import DEBUG
-from timer.maker import Timer, Task
-from model.main_model import *
 
 
 def debug():
@@ -18,13 +11,6 @@ def debug():
     debug模式
     :return:
     """
-
-    m = LittleSa()
-    m.init_sa()
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
 
     pass
 
@@ -34,6 +20,13 @@ def main_run():
     线上模式
     :return:
     """
+
+    m = LittleSa()
+    m.init_sa()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
     pass
 
 
@@ -48,19 +41,8 @@ def main():
         main_run()
     pass
 
-"""
-
-test
-
-"""
-def test_model():
-    t = Timer(settings.TASK_SETTING_PATH)
-    t.do_register()
-    pass
-
 
 if __name__ == '__main__':
-    # main()
-    #test_model()
-    debug()
+    main()
+
     pass
